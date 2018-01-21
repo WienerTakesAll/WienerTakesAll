37e5270fda5a4873529023714050ced6e257e1e2
Alfonso M <munozamb@gmail.com>
2018-01-20 18:26:08 -0700

    Increment game object id and added getters.

--- src/GameObject.h
+++ src/GameObject.h
@@ -5,7 +5,16 @@
 
 class GameObject {
 public:
-    int id_;
+    GameObject();
+    unsigned int id() const;
+    PhysicsComponent physics_component() const;
+    RenderingComponent rendering_component() const;
+
+protected:
+    unsigned int id_;
     PhysicsComponent physics_component_;
     RenderingComponent rendering_component_;
+
+private:
+    static unsigned int game_object_count;
 };
