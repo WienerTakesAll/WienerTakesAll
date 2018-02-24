8dcbae58215ba4ff50061fcb3f256bff92546749
Benjamin Stewart <benjamin.stewart1@cs.ucalgary.ca>
2018-02-24 15:12:52 -0700

    Fixed physics bug and made cameras follow all 4 cars

--- src/PhysicsSystem.cpp
+++ src/PhysicsSystem.cpp
@@ -106,23 +106,28 @@ void PhysicsSystem::update() {
 
         physx::PxBatchQuery* sq_wheel_raycast_batch_query = sq_data_->setup_batched_scene_query(g_scene_);
 
-        PxVehicleSuspensionRaycasts(
-            sq_wheel_raycast_batch_query,
-            vehicles_.size(),
-            &vehicles_[0],
-            sq_data_->get_raycast_query_result_buffer_size(),
-            sq_data_->get_raycast_query_result_buffer()
-        );
+		if (vehicles_.size())
+		{
+			PxVehicleSuspensionRaycasts(
+				sq_wheel_raycast_batch_query,
+				vehicles_.size(),
+				&vehicles_[0],
+				sq_data_->get_raycast_query_result_buffer_size(),
+				sq_data_->get_raycast_query_result_buffer()
+			);
 
 
-        physx::PxVehicleUpdates(
-            TIME_PER_UPDATE / SIM_STEPS,
-            settings_.gravity,
-            friction_pair_service_.get_friction_pairs(),
-            vehicles_.size(),
-            &vehicles_[0],
-            &vehicle_query_results[0]
-        );
+			physx::PxVehicleUpdates(
+				TIME_PER_UPDATE / SIM_STEPS,
+				settings_.gravity,
+				friction_pair_service_.get_friction_pairs(),
+				vehicles_.size(),
+				&vehicles_[0],
+				&vehicle_query_results[0]
+			);
+		}
+
+
 
         g_scene_->simulate(TIME_PER_UPDATE / SIM_STEPS);
         g_scene_->fetchResults(true);
