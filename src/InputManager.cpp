c0dd2c6dba321da3cf580f1d196f5add912feceb
Kevin <kychum@users.noreply.github.com>
2018-02-24 20:00:42 -0700

    Add keyboard support for remaining cars
    - car 0 uses wasd
    - car 1 uses tfgh
    - car 2 uses ijkl
    - car 3 uses up/left/down/right
    
    Splits up the switch statement into smaller parts
    Adds a distinction between KEYUP/BUTTONUP and KEYDOWN/BUTTONDOWN

--- src/InputManager.cpp
+++ src/InputManager.cpp
@@ -18,146 +18,39 @@ void InputManager::process_input(SDL_Event* event) {
     bool should_queue_event = true;
 
     switch (event->type) {
-        // Keyboard (Player 1)
-        case SDL_KEYDOWN: {
+        // Keyboard
+        case SDL_KEYUP: {
+            value = SDL_KEYUP;
             key = event->key.keysym.sym;
-            player_id = 0;
+            should_queue_event = process_keyboard(key, player_id);
+
+            break;
+        }
+
+        case SDL_KEYDOWN: {
+            value = SDL_KEYDOWN;
+            key = event->key.keysym.sym;
+            should_queue_event = process_keyboard(key, player_id);
+
+            break;
+        }
+
+        case SDL_CONTROLLERBUTTONUP: {
+            value = SDL_CONTROLLERBUTTONUP;
+            player_id = event->cbutton.which; // Joystick ID of event sender
+            key = event->cbutton.button;
             std::cout << "[Player " << player_id << "] ";
-
-            /* Check the SDLKey values and move change the coords */
-            switch ( event->key.keysym.sym ) {
-                case SDLK_w:
-                    std::cout << "W was pressed" << std::endl;
-                    break;
-
-                case SDLK_s:
-                    std::cout << "S was pressed" << std::endl;
-                    break;
-
-                case SDLK_a:
-                    std::cout << "A was pressed" << std::endl;
-                    break;
-
-                case SDLK_d:
-                    std::cout << "D was pressed" << std::endl;
-                    break;
-
-                case SDLK_SPACE:
-                    std::cout << "SPACE was pressed" << std::endl;
-                    break;
-
-                case SDLK_LEFT:
-                    std::cout << "Left was pressed" << std::endl;
-                    break;
-
-                case SDLK_UP:
-                    std::cout << "Up was pressed" << std::endl;
-                    break;
-
-                case SDLK_DOWN:
-                    std::cout << "Down was pressed" << std::endl;
-                    break;
-
-                case SDLK_RIGHT:
-                    std::cout << "Right was pressed" << std::endl;
-                    break;
-
-                case SDLK_F5:
-                    std::cout << "F5 was pressed" << std::endl;
-                    break;
-
-                default:
-                    should_queue_event = false;
-                    std::cout << "UNMAPPED was pressed" << std::endl;
-                    break;
-            }
+            should_queue_event = process_controller_button(key);
 
             break;
         }
 
         case SDL_CONTROLLERBUTTONDOWN: {
-            key = event->cbutton.button;
+            value = SDL_CONTROLLERBUTTONDOWN;
             player_id = event->cbutton.which; // Joystick ID of event sender
+            key = event->cbutton.button;
             std::cout << "[Player " << player_id << "] ";
-
-            switch (event->cbutton.button) {
-
-                // LETTER BUTTONS
-
-                case SDL_CONTROLLER_BUTTON_A:
-                    std::cout << "A button was pressed" << std::endl;
-                    break;
-
-                case SDL_CONTROLLER_BUTTON_B:
-                    std::cout << "B button was pressed" << std::endl;
-                    break;
-
-                case SDL_CONTROLLER_BUTTON_X:
-                    std::cout << "X button was pressed" << std::endl;
-                    break;
-
-                case SDL_CONTROLLER_BUTTON_Y:
-                    std::cout << "Y button was pressed" << std::endl;
-                    break;
-
-                // SHOULDER BUTTONS
-
-                case SDL_CONTROLLER_BUTTON_LEFTSHOULDER:
-                    std::cout << "Left bumper was pressed" << std::endl;
-                    break;
-
-                case SDL_CONTROLLER_BUTTON_RIGHTSHOULDER:
-                    std::cout << "Right bumper was pressed" << std::endl;
-                    break;
-
-                // SPECIAL BUTTONS
-
-                case SDL_CONTROLLER_BUTTON_START:
-                    std::cout << "Start button was pressed" << std::endl;
-                    break;
-
-                case SDL_CONTROLLER_BUTTON_BACK:
-                    std::cout << "Back button was pressed" << std::endl;
-                    break;
-
-                case SDL_CONTROLLER_BUTTON_GUIDE:
-                    std::cout << "Guide button was pressed" << std::endl;
-                    break;
-
-                // DIRECTIONAL BUTTONS
-
-                case SDL_CONTROLLER_BUTTON_DPAD_UP:
-                    std::cout << "D-UP button was pressed" << std::endl;
-                    break;
-
-                case SDL_CONTROLLER_BUTTON_DPAD_DOWN:
-                    std::cout << "D-DOWN button was pressed" << std::endl;
-                    break;
-
-                case SDL_CONTROLLER_BUTTON_DPAD_LEFT:
-                    std::cout << "D-LEFT button was pressed" << std::endl;
-                    break;
-
-                case SDL_CONTROLLER_BUTTON_DPAD_RIGHT:
-                    std::cout << "D-RIGHT button was pressed" << std::endl;
-                    break;
-
-                // STICK BUTTONS
-
-                case SDL_CONTROLLER_BUTTON_LEFTSTICK:
-                    std::cout << "Left stick button was pressed" << std::endl;
-                    break;
-
-                case SDL_CONTROLLER_BUTTON_RIGHTSTICK:
-                    std::cout << "Right stick button was pressed" << std::endl;
-                    break;
-
-                // MISC
-
-                default:
-                    should_queue_event = false;
-                    break;
-            }
+            should_queue_event = process_controller_button(key);
 
             break;
         }
@@ -165,54 +58,11 @@ void InputManager::process_input(SDL_Event* event) {
         case SDL_CONTROLLERAXISMOTION: {
             key = event->caxis.axis;
             value = event->caxis.value; // Current displacement of joystick
-
-            if ((value > -settings_.deadzone) && (value < settings_.deadzone)) {
-                //break;
-            }
-
             player_id = event->caxis.which; // Joystick ID of event sender
             std::cout << "[Player " << player_id << "] ";
-
-            switch (event->caxis.axis) {
-
-                // LEFT STICK
-
-                case SDL_CONTROLLER_AXIS_LEFTX:
-                    std::cout << "Left axis horizontal";
-                    break;
-
-                case SDL_CONTROLLER_AXIS_LEFTY:
-                    std::cout << "Left axis vertical";
-                    break;
-
-                // RIGHT STICK
-
-                case SDL_CONTROLLER_AXIS_RIGHTX:
-                    std::cout << "Right axis horizontal";
-                    break;
-
-                case SDL_CONTROLLER_AXIS_RIGHTY:
-                    std::cout << "Right axis vertical";
-                    break;
-
-                // TRIGGERS
-
-                case SDL_CONTROLLER_AXIS_TRIGGERLEFT:
-                    std::cout << "Left trigger";
-                    break;
-
-                case SDL_CONTROLLER_AXIS_TRIGGERRIGHT:
-                    std::cout << "Right trigger";
-                    break;
-
-                // MISC
-
-                default:
-                    should_queue_event = false;
-                    break;
-            }
-
+            should_queue_event = process_controller_axis(key, value);
             std::cout << " value: " << value << std::endl;
+
             break;
         }
 
@@ -275,4 +125,241 @@ void InputManager::handle_load_event(const Event& e) {
             std::cout << "Create AI Controller for player " << player_id << " here" << std::endl;
         }
     }
+}
+
+bool InputManager::process_keyboard(const int& key, int& player_id) {
+    bool is_key_mapped = true;
+
+    /* Check the SDLKey values and move change the coords */
+    switch ( key ) {
+        case SDLK_w:
+            player_id = 0;
+            std::cout << "W was pressed" << std::endl;
+            break;
+
+        case SDLK_s:
+            player_id = 0;
+            std::cout << "S was pressed" << std::endl;
+            break;
+
+        case SDLK_a:
+            player_id = 0;
+            std::cout << "A was pressed" << std::endl;
+            break;
+
+        case SDLK_d:
+            player_id = 0;
+            std::cout << "D was pressed" << std::endl;
+            break;
+
+        case SDLK_t:
+            player_id = 1;
+            std::cout << "T was pressed" << std::endl;
+            break;
+
+        case SDLK_f:
+            player_id = 1;
+            std::cout << "F was pressed" << std::endl;
+            break;
+
+        case SDLK_g:
+            player_id = 1;
+            std::cout << "G was pressed" << std::endl;
+            break;
+
+        case SDLK_h:
+            player_id = 1;
+            std::cout << "H was pressed" << std::endl;
+            break;
+
+        case SDLK_i:
+            player_id = 2;
+            std::cout << "I was pressed" << std::endl;
+            break;
+
+        case SDLK_j:
+            player_id = 2;
+            std::cout << "J was pressed" << std::endl;
+            break;
+
+        case SDLK_k:
+            player_id = 2;
+            std::cout << "K was pressed" << std::endl;
+            break;
+
+        case SDLK_l:
+            player_id = 2;
+            std::cout << "L was pressed" << std::endl;
+            break;
+
+        case SDLK_SPACE:
+            std::cout << "SPACE was pressed" << std::endl;
+            break;
+
+        case SDLK_LEFT:
+            player_id = 3;
+            std::cout << "Left was pressed" << std::endl;
+            break;
+
+        case SDLK_UP:
+            player_id = 3;
+            std::cout << "Up was pressed" << std::endl;
+            break;
+
+        case SDLK_DOWN:
+            player_id = 3;
+            std::cout << "Down was pressed" << std::endl;
+            break;
+
+        case SDLK_RIGHT:
+            player_id = 3;
+            std::cout << "Right was pressed" << std::endl;
+            break;
+
+        case SDLK_F5:
+            std::cout << "F5 was pressed" << std::endl;
+            break;
+
+        default:
+            is_key_mapped = false;
+            std::cout << "UNMAPPED was pressed" << std::endl;
+            break;
+    }
+
+    return is_key_mapped;
+}
+
+bool InputManager::process_controller_button(const int& button) {
+    bool is_mapped_button = true;
+
+    switch (button) {
+
+        // LETTER BUTTONS
+
+        case SDL_CONTROLLER_BUTTON_A:
+            std::cout << "A button was pressed" << std::endl;
+            break;
+
+        case SDL_CONTROLLER_BUTTON_B:
+            std::cout << "B button was pressed" << std::endl;
+            break;
+
+        case SDL_CONTROLLER_BUTTON_X:
+            std::cout << "X button was pressed" << std::endl;
+            break;
+
+        case SDL_CONTROLLER_BUTTON_Y:
+            std::cout << "Y button was pressed" << std::endl;
+            break;
+
+        // SHOULDER BUTTONS
+
+        case SDL_CONTROLLER_BUTTON_LEFTSHOULDER:
+            std::cout << "Left bumper was pressed" << std::endl;
+            break;
+
+        case SDL_CONTROLLER_BUTTON_RIGHTSHOULDER:
+            std::cout << "Right bumper was pressed" << std::endl;
+            break;
+
+        // SPECIAL BUTTONS
+
+        case SDL_CONTROLLER_BUTTON_START:
+            std::cout << "Start button was pressed" << std::endl;
+            break;
+
+        case SDL_CONTROLLER_BUTTON_BACK:
+            std::cout << "Back button was pressed" << std::endl;
+            break;
+
+        case SDL_CONTROLLER_BUTTON_GUIDE:
+            std::cout << "Guide button was pressed" << std::endl;
+            break;
+
+        // DIRECTIONAL BUTTONS
+
+        case SDL_CONTROLLER_BUTTON_DPAD_UP:
+            std::cout << "D-UP button was pressed" << std::endl;
+            break;
+
+        case SDL_CONTROLLER_BUTTON_DPAD_DOWN:
+            std::cout << "D-DOWN button was pressed" << std::endl;
+            break;
+
+        case SDL_CONTROLLER_BUTTON_DPAD_LEFT:
+            std::cout << "D-LEFT button was pressed" << std::endl;
+            break;
+
+        case SDL_CONTROLLER_BUTTON_DPAD_RIGHT:
+            std::cout << "D-RIGHT button was pressed" << std::endl;
+            break;
+
+        // STICK BUTTONS
+
+        case SDL_CONTROLLER_BUTTON_LEFTSTICK:
+            std::cout << "Left stick button was pressed" << std::endl;
+            break;
+
+        case SDL_CONTROLLER_BUTTON_RIGHTSTICK:
+            std::cout << "Right stick button was pressed" << std::endl;
+            break;
+
+        // MISC
+
+        default:
+            is_mapped_button = false;
+            break;
+    }
+
+    return is_mapped_button;
+}
+
+bool InputManager::process_controller_axis(const int& axis, const int& value) {
+    bool is_mapped_axis = true;
+
+    if ((value > -settings_.deadzone) && (value < settings_.deadzone)) {
+        //break;
+    }
+
+
+    switch (axis) {
+
+        // LEFT STICK
+
+        case SDL_CONTROLLER_AXIS_LEFTX:
+            std::cout << "Left axis horizontal";
+            break;
+
+        case SDL_CONTROLLER_AXIS_LEFTY:
+            std::cout << "Left axis vertical";
+            break;
+
+        // RIGHT STICK
+
+        case SDL_CONTROLLER_AXIS_RIGHTX:
+            std::cout << "Right axis horizontal";
+            break;
+
+        case SDL_CONTROLLER_AXIS_RIGHTY:
+            std::cout << "Right axis vertical";
+            break;
+
+        // TRIGGERS
+
+        case SDL_CONTROLLER_AXIS_TRIGGERLEFT:
+            std::cout << "Left trigger";
+            break;
+
+        case SDL_CONTROLLER_AXIS_TRIGGERRIGHT:
+            std::cout << "Right trigger";
+            break;
+
+        // MISC
+
+        default:
+            is_mapped_axis = false;
+            break;
+    }
+
+    return is_mapped_axis;
 }
