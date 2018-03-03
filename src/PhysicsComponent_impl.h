f8b64a80efcd5d5404badae24a6a24cf9dd29295
Alfonso M <14191599+ammunoz@users.noreply.github.com>
2018-03-03 14:19:35 -0700

    First try of triangle mesh

--- src/PhysicsComponent_impl.h
+++ src/PhysicsComponent_impl.h
@@ -70,12 +70,12 @@ physx::PxMaterial* PhysicsComponent<static_actor>::get_material() const {
 }
 
 template <bool static_actor>
-physx::PxConvexMesh* PhysicsComponent<static_actor>::get_mesh() const {
+auto PhysicsComponent<static_actor>::get_mesh() const {
     return g_mesh_;
 }
 
 template <bool static_actor>
-physx::PxConvexMeshGeometry* PhysicsComponent<static_actor>::get_mesh_geometry() const {
+auto PhysicsComponent<static_actor>::get_mesh_geometry() const {
     return g_mesh_geometry_;
 }
 
@@ -113,58 +113,9 @@ void PhysicsComponent<static_actor>::set_mesh(
     physx::PxCooking* cooking,
     MeshAsset* mesh
 ) {
-    physx::PxConvexMeshDesc mesh_desc;
-
-    std::vector<physx::PxVec3> phys_verts;
-    std::vector<physx::PxU32> phys_indices;
-
-    for (auto& mesh_data : mesh->meshes_) {
-        for (auto& vert : mesh_data.vertices_) {
-            physx::PxVec3 point;
-
-            point.x = vert.position_[0];
-            point.y = vert.position_[1];
-            point.z = vert.position_[2];
-
-            phys_verts.push_back(point);
-        }
-    }
-
-    for (auto& mesh_data : mesh->meshes_) {
-        for (auto& ind : mesh_data.indices_) {
-            physx::PxU32 index;
-
-            index = ind;
-
-            phys_indices.push_back(index);
-        }
-    }
-
-    mesh_desc.points.count = phys_verts.size();
-    mesh_desc.points.data = &phys_verts.front();
-    mesh_desc.points.stride = sizeof(physx::PxVec3);
-
-    mesh_desc.flags.set(physx::PxConvexFlag::eCOMPUTE_CONVEX);
-
-    assert(mesh_desc.isValid());
-
-    physx::PxDefaultMemoryOutputStream write_buffer;
-
-    bool status = cooking->cookConvexMesh(mesh_desc, write_buffer);
-
-    if (!status) {
-        return;
-    }
-
-    physx::PxDefaultMemoryInputData read_buffer(write_buffer.getData(), write_buffer.getSize());
-    g_mesh_ = physics->createConvexMesh(read_buffer);
+    create_geometry(physics, cooking, mesh);
 
     physx::PxTransform phys_transform(0, 0, 0);
-
-    // default material
-    g_material_ = physics->createMaterial(5.f, 5.f, 5.f);
-
-    g_mesh_geometry_ = new physx::PxConvexMeshGeometry(g_mesh_);
 
     g_mesh_shape_ = physics->createShape(*g_mesh_geometry_, *g_material_, true);
     create_actor(physics, phys_transform, g_mesh_shape_, 1.0f);
