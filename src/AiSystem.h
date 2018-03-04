71614aec9446749f8a97175d7ea20a8aef278893
Benjamin Stewart <benjamin.stewart1@cs.ucalgary.ca>
2018-03-03 18:49:40 -0700

    Added cars that follow player 0

--- src/AiSystem.h
+++ src/AiSystem.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <array>
+#include <glm/glm.hpp>
+#include <glm/gtx/quaternion.hpp>
+
 #include "EventSystem.h"
 
 class AiSystem : public EventSystem<AiSystem> {
@@ -9,6 +13,23 @@ public:
 
 private:
     void handle_activate_ai(const Event& e);
+	void handle_object_transform(const Event& e);
+
+	void path_to(int car, const glm::vec3& point_);
+
     int num_ai_;
-    unsigned int counter_;
+
+	struct car_data {
+		struct car_transform_data {
+			glm::vec3 position_;
+			glm::quat rotation_;
+		};
+
+		car_transform_data car_transform_;
+	};
+
+
+	std::array<car_data, 4> cars_;
+	unsigned int counter_;
+    unsigned int whos_it_;
 };
