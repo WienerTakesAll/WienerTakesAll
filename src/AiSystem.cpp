23023ba4ed47b394573383e5b053e51aef2e3235
Benjamin Stewart <benjamin.stewart1@cs.ucalgary.ca>
2018-04-07 17:28:56 -0600

    Made AI better at targetting and avoidance.

--- src/AiSystem.cpp
+++ src/AiSystem.cpp
@@ -6,6 +6,14 @@
 
 #include "AiSystem.h"
 
+namespace
+{
+    const float AVOID_DISTANCE = 5.f;
+    const float CUTOFF_MAX_RADIUS = 30.f;
+}
+
+
+
 AiSystem::AiSystem()
     : num_ai_(0)
     , counter_(0)
@@ -18,59 +26,25 @@ AiSystem::AiSystem()
 
 void AiSystem::update() {
     // only update every 10 frames
-    if (++counter_ == 10) {
+    if (++counter_ == 120) {
         counter_ = 0;
     }
 
     for (int i = 3; i >= 4 - num_ai_; i--) {
-        if (i != whos_it_) {
-            path_to(i, cars_[whos_it_].position_);
-        } else if (counter_ == 0) {
-            unsigned int random = rand() % 20;
+        choose_ai_state(i);
 
-            if (random == 0) {
-                EventSystem::queue_event(
-                    Event(
-                        EventType::KEYPRESS_EVENT,
-                        "player_id", i,
-                        "key", SDL_CONTROLLER_AXIS_LEFTX,
-                        "value", -32768
-                    )
-                );
-            } else if (random == 1) {
-                EventSystem::queue_event(
-                    Event(
-                        EventType::KEYPRESS_EVENT,
-                        "player_id", i,
-                        "key", SDL_CONTROLLER_AXIS_LEFTX,
-                        "value", 32768
-                    )
-                );
-            } else {
-                EventSystem::queue_event(
-                    Event(
-                        EventType::KEYPRESS_EVENT,
-                        "player_id", i,
-                        "key", SDL_CONTROLLER_AXIS_TRIGGERRIGHT,
-                        "value", 32768
-                    )
-                );
-            }
+        unsigned int random = rand() % 400;
 
-            random = rand() % 40;
-
-            if (random < 2) {
-                EventSystem::queue_event(
-                    Event(
-                        EventType::KEYPRESS_EVENT,
-                        "player_id", i,
-                        "key", random == 0 ? SDL_CONTROLLER_BUTTON_LEFTSHOULDER : SDL_CONTROLLER_BUTTON_RIGHTSHOULDER,
-                        "value", SDL_CONTROLLERBUTTONDOWN
-                    )
-                );
-            }
+        if (random < 2) {
+            EventSystem::queue_event(
+                Event(
+                    EventType::KEYPRESS_EVENT,
+                    "player_id", i,
+                    "key", random == 0 ? SDL_CONTROLLER_BUTTON_LEFTSHOULDER : SDL_CONTROLLER_BUTTON_RIGHTSHOULDER,
+                    "value", SDL_CONTROLLERBUTTONDOWN
+                )
+            );
         }
-
 
     }
 }
@@ -105,6 +79,130 @@ void AiSystem::handle_new_it(const Event& e) {
 }
 
 
+void AiSystem::choose_ai_state(int car)
+{
+    if (car == whos_it_)
+    {
+        int closest_car = -1;
+        float closest_car_distance = FLT_MAX;
+        for (size_t i = 0; i < 4; i++)
+        {
+            if (car == i)
+                continue;
+
+            float distance = glm::distance(cars_[car].position_, cars_[i].position_);
+            if (distance < closest_car_distance)
+            {
+                closest_car = i;
+                closest_car_distance = distance;
+            }
+        }
+
+        if (closest_car_distance < AVOID_DISTANCE)
+        {
+            avoid_car(car, closest_car);
+        }
+        else
+        {
+            random_car(car);
+        }
+    }
+    else
+    {
+        auto pos1 = cars_[car].position_;
+        auto pos2 = cars_[whos_it_].position_;
+
+        cars_[car].cutoff_cooldown_--;
+
+        if (counter_ == 0)
+        {
+            if (glm::distance(pos1, pos2*.75f) > 2.5f && cars_[car].cutoff_cooldown_ > 0)
+            {
+                cars_[car].state_ = AiState::Cutoff;
+                cars_[car].cutoff_cooldown_ = 300;
+            }
+        }
+
+        if (glm::distance(pos1, pos2*.75f) < 2.5f && cars_[car].state_ == AiState::Cutoff)
+        {
+            cars_[car].cutoff_cooldown_ = 300;
+            cars_[car].state_ = AiState::Pursue;
+        }
+
+
+
+        if(cars_[car].state_ == AiState::Cutoff)
+            cutoff_car(car, whos_it_);
+        else
+            pursue_car(car, whos_it_);
+    }
+
+
+
+}
+
+void AiSystem::avoid_car(int car, int to_avoid)
+{
+    glm::vec3 diff = cars_[to_avoid].position_ - cars_[car].position_;
+    glm::vec3 target = glm::rotate(diff, glm::radians(90.f), glm::vec3(0, 1, 0));
+
+    path_to(car, target);
+}
+
+void AiSystem::pursue_car(int car, int to_pursue)
+{
+    float aheadness = glm::distance(cars_[to_pursue].position_, cars_[car].position_);
+    glm::vec3 target = cars_[to_pursue].position_ + cars_[to_pursue].rotation_ * glm::vec3(0, 0, 0.5f + aheadness);
+
+    path_to(car, target);
+}
+
+void AiSystem::random_car(int car)
+{
+    if (counter_ % 10 == 0) {
+        unsigned int random = rand() % 20;
+
+        if (random == 0) {
+            EventSystem::queue_event(
+                Event(
+                    EventType::KEYPRESS_EVENT,
+                    "player_id", car,
+                    "key", SDL_CONTROLLER_AXIS_LEFTX,
+                    "value", -32768
+                )
+            );
+        }
+        else if (random == 1) {
+            EventSystem::queue_event(
+                Event(
+                    EventType::KEYPRESS_EVENT,
+                    "player_id", car,
+                    "key", SDL_CONTROLLER_AXIS_LEFTX,
+                    "value", 32768
+                )
+            );
+        }
+        else {
+            EventSystem::queue_event(
+                Event(
+                    EventType::KEYPRESS_EVENT,
+                    "player_id", car,
+                    "key", SDL_CONTROLLER_AXIS_TRIGGERRIGHT,
+                    "value", 32768
+                )
+            );
+        }
+    }
+}
+
+void AiSystem::cutoff_car(int car, int to_catch)
+{
+    glm::vec3 target = cars_[to_catch].position_;
+    target *= 0.75f;
+
+    path_to(car, target);
+}
+
 void AiSystem::path_to(int car, const glm::vec3& point_) {
 
     car_data& this_car = cars_[car];
@@ -117,7 +215,7 @@ void AiSystem::path_to(int car, const glm::vec3& point_) {
 
     float angle = glm::orientedAngle(vec2_facing, vec2_to);
 
-    int axis = std::min(SHRT_MAX, (int)(SHRT_MAX * (abs(angle) - 0.05)));
+    int axis = std::min(SHRT_MAX, (int)(SHRT_MAX * (abs(angle*2.5f) - 0.05)));
 
     if (angle < -0.05) {
         EventSystem::queue_event(
--- src/AiSystem.h
+++ src/AiSystem.h
@@ -16,13 +16,25 @@ private:
     void handle_object_transform(const Event& e);
     void handle_new_it(const Event& e);
 
+    void choose_ai_state(int car);
+    void avoid_car(int car, int to_avoid);
+    void pursue_car(int car, int to_pursue);
+    void random_car(int car);
+    void cutoff_car(int car, int to_catch);
+
+
     void path_to(int car, const glm::vec3& point_);
 
     int num_ai_;
 
+    enum class AiState
+    {Avoid, Pursue, Random, Cutoff};
+
     struct car_data {
         glm::vec3 position_;
         glm::quat rotation_;
+        AiState state_;
+        int cutoff_cooldown_;
     };
 
     std::array<car_data, 4> cars_;
